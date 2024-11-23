2dbb86db97b3c7d0b1db27862238af9f98cbac78
Andres Jacinto <Ahndrsx@gmail.com>
2024-11-23 16:57:14 -0600

    Frontend v1.0.2 config corrections

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import  getEnvVariables  from '../helpers/getEnvVariables';
+const {PORT} = getEnvVariables()
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Escucha en todas las interfaces
+    port: parseInt(PORT) || 3000, // Usa el puerto proporcionado o un valor predeterminado
+  },
+  build: {
+    outDir: 'dist', // Carpeta donde se generan los archivos de producción
+  },
   plugins: [react()],
 })
